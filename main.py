85475bcf54a68ac595d437b9c166a6ba210e454d
PhysicistLeon <PhysicistLeon@gmail.com>
2022-09-09 16:33:53 +0300

    fixed parameters getter in IsotropicMaterial

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ if __name__ == '__main__':
     mat = IsotropicMaterial(k=2,n=2)
     mat.parameters = [1,1]
     mat.n = 2
+    
+    print(f"{mat.n=}")
+    print(f"{mat.k=}")
+    print(f"{mat.parameters=}")
+    print(f"eps = {mat.get_permittivity(1)}")
+    
     # def merit_function(x):
     #     return x[0]**2 - x[1]**2
     
     # scipy.optimize.minimize(fun, [1,2])
-    
-    print(f"n = {mat.n}")
-    print(f"k = {mat.k}")
-    print(f"eps = {mat.get_permittivity(1)}")
